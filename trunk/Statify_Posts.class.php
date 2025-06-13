7cae31a4b4721f17cf861d521079cd647a1e56e0
Finn Dohrn <59482102+bitnulleins@users.noreply.github.com>
2025-06-13 17:33:18 +0200

    Add new version 1.3.9.

--- trunk/Statify_Posts.class.php
+++ trunk/Statify_Posts.class.php
@@ -6,7 +6,7 @@ Text Domain: statify-widget
 Author: Finn Dohrn
 Author URI: http://www.bit01.de/
 Plugin URI: http://www.bit01.de/blog/statify-widget/
-Version: 1.3.8
+Version: 1.3.9
 */
 
 require( 'Statify_Post.class.php' );
