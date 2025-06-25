e682009f1f4a44ec3f021905b9ccfd1024094385
Finn Dohrn <59482102+bitnulleins@users.noreply.github.com>
2025-06-25 15:25:36 +0200

    Reduce queries in 1.4.6 -> boost performance.

--- trunk/Statify_Posts.class.php
+++ trunk/Statify_Posts.class.php
@@ -11,37 +11,39 @@ class Statify_Posts {
 	* Return the content
 	*
 	* @since   1.0
-	* @change  1.3.1
+	* @change  1.4.6
 	*/
 	public static function get_post_ids($post_type, $post_category, $amount, $days) {
 		$posts = array();
 		$counter = 0;
-		$wpurl = parse_url(get_bloginfo('url'));
+		$wpurl = parse_url(get_bloginfo('wpurl'));
 		$targets = self::get_all_targets(intVal($days));
+		$show_on_front = get_option('show_on_front');
+		$page_for_posts = get_option( 'page_for_posts' );
 
 		foreach ($targets as $entry) {
 			$clear_url = str_replace($wpurl['host'],"",$entry['url']);
-			
+			$id = $entry['id'];
+
 			// Add "frontpage" view counter if blog view is frontpage
-			if ($clear_url == '/' && 'page' != get_option('show_on_front')) {
+			if ($clear_url == '/' && 'page' != $show_on_front) {
 				if ($post_type != 'page' && $post_type != 'postpage') continue;
 				if (!isset($posts[0])) $posts[0] = 0;
 				$posts[0] += $entry['count'];
 				continue;
 			}
 			
-			// Try to get ID from Statify Count URL
-			$id = url_to_postid(home_url( $clear_url ));
-			if ($id == 0 && 'page' == get_option('show_on_front')) {
-				$id = get_option( 'page_for_posts' );
+			// Overwrite id with selected frontpage, if option is activated
+			if ($id == 0 && 'page' == $show_on_front) {
+				$id = $page_for_posts;
 			}
 			
 			// Get page by ID
-			$page = get_page($id);
+			$page = get_post($id);
 			
 			// Find statistics for published "post&page" entries
 			if ( (isset($page->post_type) && ($page->post_type == $post_type || $post_type == 'postpage')) && (isset($page->post_status) && $page->post_status == 'publish') ) {
-                			
+
 				if (!empty($page->ID)) {
 					// When category is select, then ignore other posts!
 					if ($post_type == 'post' && $post_category > 0) {
@@ -100,6 +102,7 @@ class Statify_Posts {
 	* Returns amount of view per id
 	*
 	* @since   1.1.4
+	* @change  1.4.6
 	*/
 
 	public static function get_statify_count($post_id, $days) {
@@ -112,10 +115,7 @@ class Statify_Posts {
 		$count = 0;
 
 		foreach ($targets as $entry) {
-			$clear_url = str_replace(get_bloginfo('wpurl'),"",$entry['url']);
-			$id = url_to_postid(home_url( $clear_url ));
-
-			if ($id == $post_id) {
+			if ($entry['id'] == $post_id) {
 				$count = $entry['count'];
 				break;
 			}
@@ -128,18 +128,13 @@ class Statify_Posts {
 	* Returns all views
 	*
 	* @since   1.2
+	* @change  1.4.6
 	*/
 
 	public static function statify_count_sum($days) {
 		$wpurl = parse_url(get_bloginfo('wpurl'));
 		$targets = self::get_all_targets(intval($days));
-		$count = 0;
-
-		foreach ($targets as $entry) {
-			$count += $entry['count'];
-		}
-
-		return $count;
+		return array_sum(array_column($targets, 'count'));
 	}
 
 	/**
@@ -148,7 +143,6 @@ class Statify_Posts {
 	* @since   1.1.4
 	* @change  1.1.6
 	*/
-
 	public static function statify_count($post_id, $days) {
 		$count = self::get_statify_count($post_id, $days);
 		return $count >= 0 ? $count : 0;
@@ -201,7 +195,7 @@ class Statify_Posts {
 	* @change  1.4.6
 	*/
 
-	public static function get_all_targets($interval)
+	public static function get_all_targets($interval = 0)
 	{
 		$expiry_seconds = apply_filters( 'statify_targets_cache_expiry', STATIFY_WIDGET_DEFAULT_EXPIRATION );
 		if (!is_numeric($expiry_seconds) || $expiry_seconds <= 0) {
@@ -212,29 +206,39 @@ class Statify_Posts {
 		if ($data = get_transient(STATIFY_WIDGET_DEFAULT_TRANSIENT_PREFIX.$interval)) {
 			return $data;
 		}
-		
-		if ($interval > 0) {
-			if (function_exists('wp_timezone_string')) {
-				$timezone = new DateTimeZone(wp_timezone_string());
-			} else {
-				$timezone = new DateTimeZone(get_option('timezone_string'));
-			}
-			$datetime = new DateTime('now', $timezone);
-			$date = $datetime->modify("-{$interval} days")->format('Y-m-d');
-		}
 
 		global $wpdb;
+		
+		$query = "
+			SELECT COUNT(`target`) AS `count`, `target` AS `url`
+			FROM `$wpdb->statify`
+		";
 
 		if ($interval > 0) {
-			$data = $wpdb->get_results(
-				"SELECT COUNT(`target`) as `count`, `target` as `url` FROM `$wpdb->statify` WHERE `created` > '$date' GROUP BY `target` ORDER BY `count` DESC",
-				ARRAY_A
-			);
+			$timezone = new DateTimeZone(Statify_Posts::wp_timezone());
+			$datetime = new DateTime('now', $timezone);
+			$date = $datetime->modify("-{$interval} days")->format('Y-m-d');
+			$query .= "WHERE `created` > '$date'\n";
+		}
+
+		$query .= "
+			GROUP BY `target`
+			ORDER BY `count` DESC
+		";
+
+		$data = $wpdb->get_results($query, ARRAY_A);
+
+		if ($interval > 0) {
+			$map = array_column(self::get_all_targets(), 'id', 'url');
+			foreach ($data as &$item) {
+				$item['id'] = $map[$item['url']] ?? 0;
+			}
+			unset($item);
 		} else {
-			$data = $wpdb->get_results(
-				"SELECT COUNT(`target`) as `count`, `target` as `url` FROM `$wpdb->statify` GROUP BY `target` ORDER BY `count` DESC",
-				ARRAY_A
-			);
+			foreach ($data as &$item) {
+				$item['id'] = ($item['url'] == '/') ? 0 : url_to_postid($item['url']);
+			}
+			unset($item);
 		}
 		
 		set_transient(
@@ -243,5 +247,31 @@ class Statify_Posts {
 
 		return $data;
 	}
+	
+	/**
+	 * Copy of wp_timezone_string() for fallback < WP v5.3
+	 * @since 1.4.6
+	*/
+	private static function wp_timezone() {
+		if (function_exists('wp_timezone_string')) {
+			return wp_timezone_string();
+		}
+		
+		$timezone_string = get_option( 'timezone_string' );
 
+		if ( $timezone_string ) {
+			return $timezone_string;
+		}
+
+		$offset  = (float) get_option( 'gmt_offset' );
+		$hours   = (int) $offset;
+		$minutes = ( $offset - $hours );
+
+		$sign      = ( $offset < 0 ) ? '-' : '+';
+		$abs_hour  = abs( $hours );
+		$abs_mins  = abs( $minutes * 60 );
+		$tz_offset = sprintf( '%s%02d:%02d', $sign, $abs_hour, $abs_mins );
+
+		return $tz_offset;
+	}
 }
